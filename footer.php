20360e670d851c97f1fab17c165ab291e6bc6f8e
vonKristoff <mrjcnicolas@gmail.com>
2013-08-01 16:25:34 +0100

    bite typo

--- footer.php
+++ footer.php
@@ -59,7 +59,7 @@
   // m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   // })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  // ga('create', 'UA-40858550-3', 'bite-software.com');
+  // ga('create', 'UA-40858550-3', 'bite-software.co.uk');
   // ga('send', 'pageview');
 
 </script>
